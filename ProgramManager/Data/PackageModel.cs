75437725ec11bf215542f740836672ce043dc206
Mantilas <lexa1th@gmail.com>
2018-09-05 06:32:36 +0800

    Initial project

---
+++ ProgramManager/Data/PackageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramManager.Data
+{
+    class PackageModel
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Version { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
+    }
+}
