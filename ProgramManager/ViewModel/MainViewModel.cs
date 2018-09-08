844ab30bfdf2e9ce8b3ba5bbda0090ed91956b0f
Mantilas <lexa1th@gmail.com>
2018-09-08 18:38:42 +0800

    Little changes in the MainViewModel file.

--- ProgramManager/ViewModel/MainViewModel.cs
+++ ProgramManager/ViewModel/MainViewModel.cs
@@ -11,12 +11,13 @@ namespace ProgramManager.ViewModel
     {
         private PackageModel _currentPackage;
         private CategoryModel _currentSubcategory;
-        private ObservableCollection<PackageModel> _package;
-        private ObservableCollection<CategoryModel> _category;
-        private ObservableCollection<CategoryModel> _subcategory;
         private CategoryModel _currentCategory;
+        private ObservableCollection<PackageModel> _package;
         public ObservableCollection<CategoryModel> Subcategory { get; set; }
         public ObservableCollection<CategoryModel> Category { get; set; }
+        /// <summary>
+        /// Receives and transfers information on the current packet to view
+        /// </summary>
         public ObservableCollection<PackageModel> Packages
         {
             get { return _package; }
@@ -26,10 +27,8 @@ namespace ProgramManager.ViewModel
                 OnPropertyChanged("Packages");
             }
         }
-        //public ICommand InsertCommand { get; }
-        //public ICommand UpdateCommand { get; }
         /// <summary>
-        /// Получает и передает в view информацию о текущем пакете
+        /// Receives and transfers information on the current category to view 
         /// </summary>
         public PackageModel CurrentPackage
         {
@@ -52,14 +51,19 @@ namespace ProgramManager.ViewModel
                 {
                     _currentCategory = value;
 
+                    // Request of these Subcategories
                     Subcategory = new ObservableCollection<CategoryModel>(CategoryAccess.GetSubcategories());
-
+                    
+                    // Filters subcategory depending on the selected category.
                     IEnumerable<CategoryModel> query = Subcategory.Where(package => package.CategoryName == _currentCategory.CategoryName);
 
+                    // Creating new data
                     Subcategory = new ObservableCollection<CategoryModel>(query);
-
+                    
+                    // Adding special elements.
                     Subcategory.Insert(0, new CategoryModel() { SubcategoryName = "Все" });
 
+                    // Updating properties Subcategory
                     OnPropertyChanged("Subcategory");
                 }   
             }
@@ -72,43 +76,36 @@ namespace ProgramManager.ViewModel
                 if (_currentSubcategory != value && value != null)
                 {
                     _currentSubcategory = value;
-                    
-                    // TODO: Оптимизировать эту строку(Эта строка повтроно запрашивает данные что может повлиять на производительность программы)
+                    // TODO: To optimize this line (This line repeatedly requests data that can affect program performance)
                     Packages = new ObservableCollection<PackageModel>(PackageAccess.GetPackages());
 
-                    // Фильтрует данные взависимости от выбранной подкотегории
+                    // Filters data depending on the selected subcategory if the special element "All" is selected, all data of subcategories will be selected
                     if (_currentSubcategory.SubcategoryName != "Все")
                     {
                         IEnumerable<PackageModel> query = Packages.Where(package => package.Subcategory == _currentSubcategory.SubcategoryName);
                         Packages = new ObservableCollection<PackageModel>(query);
                     }
-                    if(_currentSubcategory.SubcategoryName == "Все")
+                    // Filters data depending on the selected category and displays the list of all these subcategories
+                    else
                     {
                         IEnumerable<PackageModel> query = Packages.Where(package => package.Category == _currentCategory.CategoryName);
                         Packages = new ObservableCollection<PackageModel>(query);
-                    }
-                    
+                    }                    
                     if (Packages.Count > 0)
                     {
-                        // Указатель на перевый элемент списка для корректной работы информационной панели 
+                        // Selects the first element of the list for correct work of a data panel
                         _currentPackage = Packages[0];
-                        // Обновление свойства текущего пакета
+                        // Updating of property of the current package
                         OnPropertyChanged("CurrentPackage");
-                    }
-                    OnPropertyChanged("CurrentSubcategory");                 
+                    }               
                 }                          
             }
         }
         /// <summary>
-        /// Конструктор поумолчанию
+        /// Default constructor
         /// </summary>
         public MainViewModel()
         {
-            //UpdateCommand = new RelayCommand(this, Update);
-            //InsertCommand = new RelayCommand(this, Insert);
-
-            //Packages = new ObservableCollection<PackageModel>(PackageAccess.GetPackages());
-            //Subcategory = new ObservableCollection<CategoryModel>(CategoryAccess.GetSubcategories());
             Category = new ObservableCollection<CategoryModel>(CategoryAccess.GetCategories());
         }
     }
